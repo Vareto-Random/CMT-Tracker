d181f02025721b55837f0dca4b68487a01ea9c13
Rafael Vareto <rafaelvareto@gmail.com>
2016-07-22 10:13:20 -0300

    SAT meeting 22-07-2016

--- VaretoCMT/VARpool.py
+++ VaretoCMT/VARpool.py
@@ -17,6 +17,8 @@ import VARtracker
 #         return getattr, (m.im_self, m.im_func.func_name)
 # copy_reg.pickle(types.MethodType, _reduce_method)
 
+output = mp.Queue()
+
 
 def main():
     # print('VARpool(../video_carlos/, 1000, [[140, 170]], [[300, 500]])')
@@ -25,17 +27,22 @@ def main():
     # VARmethod('../video_tennis/', 1000, [[405, 160]], [[450, 275]])
     # print('VARpool(../video_tennis/, 1000, [[405, 160],[255, 100]], [[450, 275],[275, 155]])')
     # VARmethod('../video_tennis/', 1000, [[405, 160],[255, 100]], [[450, 275],[275, 155]])
+    # print('VARpool(../video_tennis/, 1000, [[405, 160],[255, 100],[340,80]], [[450, 275],[275, 155],[355,115]])')
+    # VARmethod('../video_tennis/', 500, [[405, 160],[255, 100],[340,80]], [[450, 275],[275, 155],[355,115]])
+    # print('VARpool(../video_tennis/, 1000, [[405, 160],[255, 100],[340,80]], [[450, 275],[275, 155],[355,115]])')
+    # VARmethod('../video_tennis/', 400, [[405, 160],[255, 100],[340,80],[270,80]], [[450, 275],[275, 155],[355,115],[280,103]])
     print('VARpool(../video_tennis/, 1000, [[405, 160],[255, 100],[340,80]], [[450, 275],[275, 155],[355,115]])')
-    VARmethod('../video_tennis/', 200, [[405, 160],[255, 100],[340,80]], [[450, 275],[275, 155],[355,115]])
+    VARmethod('../video_tennis/', 1000, [[405, 160],[255, 100],[340,80],[270,80],[245,75]], [[450, 275],[275, 155],[355,115],[280,103],[253,99]])
 
 
-def worker(folder_path, list_name, top_left, bot_right):
+def worker(folder_path, list_name, top_left, bot_right, index):
     frame_path = folder_path + '/' + list_name[0]
     image_0 = cv.imread(frame_path)
     gray_0 = cv.cvtColor(image_0, cv.COLOR_BGR2GRAY)
 
     cmt = VARtracker.CMT()
     cmt.initialise(gray_0, top_left, bot_right)
+    box_queue = mp.Queue()
 
     for name in list_name:
         frame_path = folder_path + '/' + name
@@ -43,14 +50,10 @@ def worker(folder_path, list_name, top_left, bot_right):
         gray_now = cv.cvtColor(image_now, cv.COLOR_BGR2GRAY)
 
         cmt.process_frame(gray_now)
-        print 'Iterando'
-    print 'Saiu'
-
-
-queue = mp.Queue()
-def on_return(result):
-    queue.put(result)
-    print 'Saiu'
+        if cmt.has_result:
+            print index, name, zip(cmt.tl, cmt.br)
+            output.put((index, name, zip(cmt.tl, cmt.br)))
+    print 'Processo {} terminou'.format(index)
 
 
 def VARmethod(folder_path, final_frame, top_left, bot_right):
@@ -60,16 +63,16 @@ def VARmethod(folder_path, final_frame, top_left, bot_right):
         list_frame = [index for index in range(1, final_frame + 1)]
         list_name = [str(index) + '.jpg' for index in list_frame]
 
-        pool = mp.Pool(3)
-        for item in zip(top_left, bot_right):
-            pool.apply_async(worker, args=(folder_path, list_name, item[0], item[1]))
+        pool = mp.Pool(5)
+        for index in range(0, len(top_left)):
+            pool.apply_async(worker, args=(folder_path, list_name, top_left[index], bot_right[index], index))
         pool.close()
         pool.join()
-
 
         print 'Finished with the script'
 
     toc = time.time()
+    print output.qsize()
     print (toc - tic)
 
 
